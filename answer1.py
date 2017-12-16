f5a92a2c85f552ef1e866bc9600e1d5203ed3863
Norrris <1155064682@link.cuhk.edu.hk>
2017-12-16 16:52:12 +0800

    Add files via upload

--- answer1.py
+++ answer1.py
@@ -1,18 +1,18 @@
 #Run in python 3
+#Initialize 2 list(listA is the set,list B is the testing list) and split the list by ,
 a=input("list A\n")
 a=a.split(',')
 b=input("list B\n")
 b=b.split(',')
 check=0
 
+# If there are same element,check+1
 for i in range(len(a)):
 	for j in range(len(b)):
 		if a[i]==b[j]:
 			check+=1
-		else:
-			check+=0
 
-
+# If ckheck is larger or equal to length of the subset list, it means it is the subset of list A,else it is not.
 if check>=len(b):
 	print("true")
 
--- answer3.py
+++ answer3.py
@@ -2,19 +2,23 @@
 a=1
 b=1
 f=[]
+# Append a list to store input and change input to be integer
 list=input("Numbers:")
 list=list.split(',')
 list = [ int(x) for x in list ]
 
+#prepare a list to store the first 60 fibonacci numbers
 for i in range(60):
 	c=a+b
 	a=b
 	b=c
 	f.append(c)
 
+# If input list include 1 then print 2
 if 1 in list:
 	print (2)
-
+ 
+ #Check input in list>2,check if the number is within the boundary of f[i] and f[i+1],if yes,then print out f[i+1]
 for j in range(len(list)):
 	for i in range(len(f)):
 		if (list[j]>f[i] and list[j]<f[i+1]) or list[j]==f[i]:
